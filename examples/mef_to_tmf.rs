79d8e8363458aec4e1ed76f673ab12e5680a3594
Ryan Ruckley <rruckley@gmail.com>
2024-07-29 14:22:56 +1000

    add more data

--- examples/mef_to_tmf.rs
+++ examples/mef_to_tmf.rs
@@ -2,10 +2,22 @@
 
 use mef2tmf::mef_to_tmf_site;
 use meflib::w122::geographic_site::GeographicSite as MEFSite;
+use meflib::w122::geographic_site::{RelatedContactInformation,FieldedAddress};
 
 fn main() {
+    let contact_info = RelatedContactInformation {
+        name : "John Q. Citizen".to_string(),
+        ..Default::default()
+    };
+
+    let address = FieldedAddress::default();
     let mut mef = MEFSite::default();
     mef.name = "MEF Name".to_string();
+    mef.description = "MEF Description".to_string();
+    mef.postal_address = vec![address];
+    mef.company_name = "Building Owner Name".to_string();
+    mef.company_name = "Tenant Name".to_string();
+    mef.related_contact_information = vec![contact_info];
 
     let tmf = mef_to_tmf_site(mef);
 
